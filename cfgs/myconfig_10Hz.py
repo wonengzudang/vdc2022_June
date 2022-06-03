a662d3420659cd053ba3e95fc5806bc5deb9a450
Takehiro_Watanabe <s1270148@u-aizu.ac.jp>
2022-06-03 15:26:01 +0900

    add

--- cfgs/myconfig_10Hz.py
+++ cfgs/myconfig_10Hz.py
@@ -32,10 +32,10 @@ DONKEY_SIM_PATH = "./DonkeySimLinux/donkey_sim.x86_64"
 SIM_HOST = "localhost"
 
 DONKEY_GYM_ENV_NAME = "donkey-avc-sparkfun-v0"
-GYM_CONF = { "body_style" : "donkey", "body_rgb" : (230, 0, 50), "car_name" : "Ahoy!", "font_size" : 18} # body style(donkey|bare|car01) body rgb 0-255
+GYM_CONF = { "body_style" : "donkey", "body_rgb" : (230, 0, 50), "car_name" : "jp_beef", "font_size" : 18} # body style(donkey|bare|car01) body rgb 0-255
 
 
-GYM_CONF["racer_name"] = "hirohaku"
+GYM_CONF["racer_name"] = "huang"
 GYM_CONF["country"] = "JP"
 GYM_CONF["bio"] = "HELLO"
 
